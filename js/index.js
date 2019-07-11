75cdb197bf423561f8aff31f029d16d50fd78c9e
spektordude <spektordude@gmail.com>
2019-07-12 01:40:20 +0200

    Project Updates

--- js/index.js
+++ js/index.js
@@ -1,39 +1,25 @@
 Vue.component('app-header', {
 
-  template: `<header class="sticky">
-
-                      <a href="#" class="logo">Viktor Hura</a>
-
-                      <label for="modal-control1" class="button">Discord</label>
-                      <input type="checkbox" id="modal-control1" class="modal">
-                      <div>
-                        <div class="card header-modal">
-                          <label for="modal-control1" class="modal-close" ></label>
-                          <h3 class="section">Discord username</h3>
-
-                          <input id="discord" type="text" value="Viktor#1386">
-                          <button type="button" onclick='document.getElementById("discord").select();document.execCommand("copy");'>Copy</button>
-
-                        </div>
+  template: `<div class="sticky bordered">
+                    <div class="row">
+                    <div class="col-sm-fluid ">
+                      <a href="#" class="logo" style="color:white;"><h3>Viktor Hura</h3></a>
                       </div>
 
-                      <a href="https://steamcommunity.com/id/penguinsniffer/" target="_blank" class="button">Steam</a>
-
-                      <label for="modal-control2" class="button">Email</label>
-                      <input type="checkbox" id="modal-control2" class="modal">
-                      <div>
-                        <div class="card header-modal">
-                          <label for="modal-control2" class="modal-close" ></label>
-                          <h3 class="section">Email address</h3>
-                          <input id="email" type="text" value="viktorhura.info@gmail.com">
-                          <button type="button" onclick='document.getElementById("email").select();document.execCommand("copy");'>Copy</button>
-
-                        </div>
+                      <div class="col-sm-fluid ">
+                        <button class="inverse shadowed" onclick="prompt('Discord', 'Viktor#1386')">Discord</button>
+                      </div>
+                      <div class="col-sm-fluid ">
+                      <a href="https://steamcommunity.com/id/penguinsniffer/" target="_blank" class="button inverse shadowed">Steam</a>
+                      </div>
+                      <div class="col-sm-fluid ">
+                        <button class="inverse shadowed" onclick="prompt('Email', 'viktorhura.info@gmail.com')">Email</button>
                       </div>
 
 
+                      </div>
 
-              </header>
+              </div>
               `
 
 });
@@ -56,7 +42,7 @@ Vue.component('blog', {
 
 Vue.component('blog-post', {
   props: ['title', 'date', 'image', 'link', 'description'],
-  template: `<div class="container">
+  template: `<div :id="date" class="container">
 
              <div class="row cols-sm-12">
               <h2>{{ title }}
@@ -86,7 +72,83 @@ var app = new Vue({
   el: '#app',
   data: {
     blogposts: [{
-        id: 1,
+        id: 9,
+        title: 'Multilayer Perceptron Network using Genetic Algorithms in Java, or mlpnet-ga-java abbreviated',
+        date: '9/7/2019',
+        image: "img/Mlpnet-ga-java.png",
+        link: "https://github.com/ViktorHura/mlpnet-ga-java",
+        description: `
+                        A 3 layer MLP Network, using Genetic Algorithm as a trainer, to solve XOR problem</br>
+                        I first wrote this program in golang, now rewrote it to learn java. <a href="#17/4/2019">Original Go version</a>
+                        `
+      },
+      {
+        id: 8,
+        title: 'Znimok',
+        date: '26/4/2019',
+        image: "img/Znimok.png",
+        link: "https://github.com/ViktorHura/znimok",
+        description: `
+                            Znimok - yet another screenshot application, written in go</br></br>
+
+                            Using <a href="https://github.com/bamontelucas/image-clipboard">https://github.com/bamontelucas/image-clipboard</a> to copy images due to limitations in go</br>
+                            Usage: Make a selection with mouse and press Space to copy to clipboard</br>
+                            Works on multiple displays as long as the tops of the displays are aligned</br>
+                          `
+      },
+      {
+        id: 7,
+        title: 'Multilayer Perceptron Network using Genetic Algorithms in Go, or mlpnet-ga-go abbreviated',
+        date: '17/4/2019',
+        image: "img/Mlpnet-ga-go.png",
+        link: "https://github.com/ViktorHura/mlpnet-ga-go",
+        description: `
+                        A 3 layer MLP Network, using Genetic Algorithm as a trainer, to solve XOR problem. </br>Written in Go.
+                        </br></br> I use the input-layer matrix and the hidden-layer matrix as "chromosones" in my genetic algorithm to evolve them to solve the XOR problem.
+                        `
+      },
+      {
+        id: 6,
+        title: 'Genetic Image',
+        date: '23/3/2019',
+        image: "img/GeneticImage.png",
+        link: "https://github.com/ViktorHura/genetic-image-go",
+        description: `
+                          I wrote this program in Golang, to recreate an image using polygons. I use "<a href="https://en.wikipedia.org/wiki/Genetic_algorithm">Genetic Algorithm</a>" to
+                          evolve a "population of images" to look as similar as possible to my original image.</br></br>
+
+                          <figure>
+                          <a :href="link"><img src="img/GeneticImage2.png" alt="Image unavailable"/></a>
+                          <figcaption><a href="https://github.com/ViktorHura/genetic-image-go/tree/master/logs/Mona%20Lisa%20Klein"><h5><strong>Link</strong><span class="icon-link"></span></h5></a></figcaption>
+                          </figure>
+
+                          I got inspired by <a href="https://rogerjohansson.blog/2008/12/07/genetic-programming-evolution-of-mona-lisa/">Roger Johansson's blog post from 2008</a>, so I decided to write a similar program in Go.</br></br>
+                          My approach is different in a few ways. My algorithm is a "true" genetic algorithm, where you need two parents to create an offspring which in turn also receives a mutation.
+                          Roger Johansson's algorithm uses "asexual reproduction", using only mutation, to create the next generation.</br></br>
+                          I found that I achieve similar results but with only 1/5th of the iterations needed compared to Roger Johansson, but you must remember that his "populations" only contain 2 individuals while I have 200.</br>
+                          This makes the amount of time to compute one of my generations compared to his also much longer.  </br></br>
+                          In the end I don't receive as good results as Roger Johansson. </br>
+                          This is mostly because I have a fixed amount of polygons while in Rogers algorithm the organism starts out with only a few and gains more during evolution. Furthermore, my mutation algorithm is not granular enough, which is important in later stages of evolution where you only need small changes instead of strong mutations.</br></br>
+                          I use tournament selection, elitism, cutoff and 1 point crossover for my algorithm. If you're interested in more details feel free to checkout my sourcecode at the link above.
+                          `
+      },
+      {
+        id: 5,
+        title: 'Newton Fractal',
+        date: '12/2/2019',
+        image: "img/NewtonFractal.png",
+        link: "https://github.com/ViktorHura/newtonrhapson",
+        description: `
+                        I applied "<a href="https://en.wikipedia.org/wiki/Newton%27s_method">Newton's Method</a>" in javascript with real and imaginary numbers.</br></br>
+                        The example above shows fractals generated by applying the method to an imaginary number. This number represent a pixel on screen, where the x-axis shows the real part
+                        and the y-axis shows the imaginary part. </br></br>We determine the brightness of the pixel by applying Newton's method to the given formula, using this number as a starting guess
+                        and counting how many iterations we need to reach an answer. The more iterations are needed, the brighter the pixel. Red pixels didn't reach an answer within the given max iteration limit.
+
+                        </br></br> This was made for a school project.
+                        `
+      },
+      {
+        id: 4,
         title: '4D Cube (Tesseract)',
         date: '15/2/2018',
         image: "img/4D.png",
@@ -97,7 +159,7 @@ var app = new Vue({
                       `
       },
       {
-        id: 2,
+        id: 3,
         title: 'AirDashGL',
         date: '29/10/2017',
         image: "img/AirDashGL.png",
@@ -105,7 +167,7 @@ var app = new Vue({
         description: `A fun 2D flying game rendered in 3D, made using <a href="https://godotengine.org/">Godot 2.2</a>.`
       },
       {
-        id: 3,
+        id: 2,
         title: 'KveikGL',
         date: '01/09/2017',
         image: "img/KveikGL.png",
@@ -114,9 +176,9 @@ var app = new Vue({
                     It is built using <a href="https://nunustudio.org/">NunuStudio</a>`
       },
       {
-        id: 4,
+        id: 1,
         title: 'Fall Of Adolphin',
-        date: '19/03/20177',
+        date: '19/03/2017',
         image: "img/FOA.png",
         link: "https://viktorhura.github.io/Fall-Of-Adolphin/",
         description: `My first attempt at a Webgl Adventure Game written in Three.js mostly from scratch using <a href="https://www.createjs.com/soundjs">SoundJs</a> for audio.
